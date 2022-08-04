d433953544d92e0d98f4415f64a5950fb7d28263
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-08-04 20:09:58 +0700

    1.6

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { createApp, reactive } from 'vue'
 
-createApp(App).use(store).use(router).mount('#app')
+//Create a reactive object
+const GStore = reactive({ flashMessage: '' })
+createApp(App).use(store).use(router).provide('GStore', GStore).mount('#app')
